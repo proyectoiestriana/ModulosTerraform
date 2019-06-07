489fbc0cb2562021550440dbfc88d9ccc309261e
Juan Moreno <juanmogal20@gmail.com>
2019-06-07 10:14:09 +0200

    Update modulogithub.tf

--- modulogithub.tf
+++ modulogithub.tf
@@ -3,8 +3,8 @@ provider "github" {
   token        = "0695001343ca23201c5e265aca193194d96a42bb"
   organization = "proyectoiestriana"
 }
-resource "github_repository" "RepositorioModulado" {
-  name        = "RepositorioModulado"
-  description = "Creando repositorios con modulos de Terraform"
+resource "github_repository" "RepositorioModuladoVolcandoCodigo" {
+  name        = "RepositorioModuladoVolcandoCodigo"
+  description = "Creando repositorios con código volcado desde Terraform"
   private = false
 }
